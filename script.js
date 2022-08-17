0e67f052fa3eebd2f0a81e1f8dcbbee658cbf086
Zac Smart <zac_chaz@outlook.com>
2022-08-17 20:48:20 +1000

    added if argument password length

--- script.js
+++ script.js
@@ -3,10 +3,36 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var uppercase = []
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
-var characters = ['+', '-', '!', '@', '#', '%', '&', '*', '?']
+var specialCharacters = ['+', '-', '!', '@', '#', '%', '&', '*', '?']
+
 function generatePassword() {
-  // 
+  var passwordLength = window.prompt("Enter desired password length");
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    console.log("invalid")
+    window.alert("To generate password please choose a number beyween 8 and 128.")
+    return;
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
