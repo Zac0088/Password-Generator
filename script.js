00bab402be3453906e56c6e64f66acefe46fd53a
Zac Smart <zac_chaz@outlook.com>
2022-08-18 22:06:27 +1000

    saved boolean staements inside variables to work with for loops but still couldnt get desired result

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Co
 var generateBtn = document.querySelector("#generate");
-var lowerCase = ['abcdefghijklmnopqrstuvwxyz']
+var lowerCase = ['a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var upperCase = ['ABCDEFGHIjKLMNOPQRSTUVWXYZ']
 var numbers = ['0123456789']
 var specialCharacters = ['+-!@#%&*?']
@@ -14,42 +14,82 @@ function generatePassword() {
   );
 
   if (passwordLength < 8 || passwordLength > 128) {
-    // console.log("invalid")
+
     window.alert("To generate password please choose a number beyween 8 and 128.");
     return generatePassword();
   }
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    var lowerCaseConfirm = window.confirm("Include lowercase letters? (ook for yes, cancel for no)");
-    var upperCaseConfirm = window.confirm("Include upperclase letters?(ok for yes, cancel for no)");
-    var specialCharactersConfirm = window.confirm("Include special characters?(ok for yes, cancel for no)");
-    var numbersConfirm = window.confirm("Include numbers?(ok for yes, cancel for no)");
-  }
 
-  var password = "";
-  for (var index = 0; index < passwordLength; index++) {
-    var trueRandom =
-      newPassword[Math.floor(Math.random() * newPassword.length)];
-    password += trueRandom;
+
+  var lowerCaseConfirm = window.confirm("Include lowercase letters? (ook for yes, cancel for no)")
+  if (lowerCaseConfirm) {
+    console.log("true");
+  } else {
+    console.log("false");
   }
-  return password;
+  var upperCaseConfirm = window.confirm("Include upperclase letters?(ok for yes, cancel for no)")
+  if (upperCaseConfirm) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+  var specialCharactersConfirm = window.confirm("Include special characters?(ok for yes, cancel for no)")
+  if (specialCharactersConfirm) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+  var numbersConfirm = window.confirm("Include numbers?(ok for yes, cancel for no)")
+  if (numbersConfirm) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+  if ((lowerCaseConfirm) && (upperCaseConfirm) && (specialCharactersConfirm) && (numbersConfirm)) {
+    let result = "";
+    for (let index = 0; index < passwordLength; index++) {
+      result += (lowerCase + upperCase + numbers + specialCharacters).charAt(Math.floor(Math.random() * (lowerCase + upperCase + numbers + specialCharacters).length));
+    }
+    return result;
+    }
+   else if ((!lowerCaseConfirm) && (upperCaseConfirm) && (specialCharactersConfirm) && (numbersConfirm)) {
+      let result ="";
+      for (let index=0; index < passwordLength; index++) {
+        result += (lowerCase + upperCase + numbers + specialCharacters).charAt(Math.floor(Math.random() * (lowerCase + upperCase + numbers + specialCharacters).length));
+      }
+      return result;
+    }
+    else if ((lowerCaseConfirm) && (!upperCaseConfirm) && (specialCharactersConfirm) && (numbersConfirm)) {
+      let result ="";
+      for (let index=0; index < passwordLength; index++) {
+        result += (lowerCase + upperCase + numbers + specialCharacters).charAt(Math.floor(Math.random() * (lowerCase + upperCase + numbers + specialCharacters).length));
+      }
+      return result;
+    }
+    else if ((lowerCaseConfirm) && (upperCaseConfirm) && (!specialCharactersConfirm) && (numbersConfirm)) {
+      let result ="";
+      for (let index=0; index < passwordLength; index++) {
+        result += (lowerCase + upperCase + numbers + specialCharacters).charAt(Math.floor(Math.random() * (lowerCase + upperCase + numbers + specialCharacters).length));
+      }
+      return result;
+    }
+    else if ((lowerCaseConfirm) && (upperCaseConfirm) && (specialCharactersConfirm) && (!numbersConfirm)) {
+      let result ="";
+      for (let index=0; index < passwordLength; index++) {
+        result += (lowerCase + upperCase + numbers + specialCharacters).charAt(Math.floor(Math.random() * (lowerCase + upperCase + numbers + specialCharacters).length));
+      }
+      return result;
+    }
+  
+  }
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-
-
-
-
-
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
-
-  }
-
-
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
